7a087f2971adfb40cd7c3c39c0f94a26b18a90b4
eve1243 <Everlyn.Njeri@stud.fh-campuswien.ac.at>
2026-09-09 16:31:19 +0200

    demo 9

--- js/api.js
+++ js/api.js
@@ -18,30 +18,24 @@ import { renderTimeline } from "./timeline.js";
   }
 }
 
-export function loadCorePeopleAndLocations() {
-  return fetch("data/case.json").then(function (caseRes) {
-    return caseRes.json().then(function (caseJson) {
-      Object.assign(state.caseData, caseJson);
+export async function loadCorePeopleAndLocations() {
+  const caseRes = await fetch("data/case.json");
+  const caseJson = await caseRes.json();
+  Object.assign(state.caseData, caseJson);
 
-      return fetch("data/people.json").then(function (peopleRes) {
-        return peopleRes.json().then(function (peopleJson) {
-          state.allPeople.length = 0;
-          state.allPeople.push.apply(state.allPeople, peopleJson);
+  const peopleRes = await fetch("data/people.json");
+  const peopleJson = await peopleRes.json();
+  state.allPeople.length = 0;
+  state.allPeople.push.apply(state.allPeople, peopleJson);
 
-          return fetch("data/locations.json").then(function (locationsRes) {
-            return locationsRes.json().then(function (locationsJson) {
-              state.allLocations.length = 0;
-              state.allLocations.push.apply(state.allLocations, locationsJson);
+  const locationsRes = await fetch("data/locations.json");
+  const locationsJson = await locationsRes.json();
+  state.allLocations.length = 0;
+  state.allLocations.push.apply(state.allLocations, locationsJson);
 
-              hideLoadingStep();
-              renderDashboard();
-              populateAllDropdowns();
-            });
-          });
-        });
-      });
-    });
-  });
+  hideLoadingStep();
+  renderDashboard();
+  populateAllDropdowns();
 }
 
 export function loadEvidenceData() {
@@ -71,34 +65,29 @@ export function loadEvidenceData() {
     });
 }
 
-export function loadTimelineData() {
-  return fetch("data/timeline.json")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      state.allTimeline.length = 0;
-      state.allTimeline.push.apply(state.allTimeline, data);
+export async function loadTimelineData() {
+  try {
+    const res = await fetch("data/timeline.json");
+    const data = await res.json();
+    state.allTimeline.length = 0;
+    state.allTimeline.push.apply(state.allTimeline, data);
 
-      renderDashboard();
-      if (state.currentPage === "timeline") {
-        renderTimeline();
-      }
-      populateAllDropdowns();
-    })
-    .catch(function (err) {
-      console.log("timeline load error", err);
-    })
-    .finally(function () {
-      hideLoadingStep();
-    });
+    renderDashboard();
+    if (state.currentPage === "timeline") {
+      renderTimeline();
+    }
+    populateAllDropdowns();
+  } catch (err) {
+    console.log("timeline load error", err);
+  } finally {
+    hideLoadingStep();
+  }
 }
 
-export function loadAllData() {
+export async function loadAllData() {
   showLoadingOverlay("Loading case file…");
   state.loadingStepsRemaining = 2;
-  return loadCorePeopleAndLocations().then(function () {
-    return Promise.all([loadEvidenceData(), loadTimelineData()]);
-  });
+  await loadCorePeopleAndLocations();
+  await Promise.all([loadEvidenceData(), loadTimelineData()]);
 }
 
